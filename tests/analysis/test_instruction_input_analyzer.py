c7ae5d3d2dd66bd90cdf651600aa1969a7f57a7a
A_A <21040751+Otto-AA@users.noreply.github.com>
2024-03-22 18:37:00 +0100

    feat: prepare output of InstructionInputAnalyzer

--- tests/analysis/test_instruction_input_analyzer.py
+++ tests/analysis/test_instruction_input_analyzer.py
@@ -2,7 +2,8 @@ from itertools import zip_longest
 from traces_analyzer.analysis.InstructionInputAnalyzer import InstructionInputAnalyzer
 from traces_analyzer.call_frame import CallFrame
 from traces_analyzer.instructions import CALL, STOP, Unknown
-from traces_analyzer.trace_reader import TraceEvent
+from traces_analyzer.parser import parse_events
+from traces_analyzer.trace_reader import TraceEvent, read_trace_file
 
 
 def test_instruction_input_analyzer():
@@ -34,15 +35,60 @@ def test_instruction_input_analyzer():
         Unknown(TraceEvent(2, 0xEE, [], 1), dummy_event, child_frame),
         STOP(dummy_event, dummy_event, child_frame),
     ]
+
+    # run analysis
     analyzer = InstructionInputAnalyzer()
     for a, b in zip_longest(first_trace, second_trace):
         analyzer.on_instructions(a, b)
 
-    # the two calls differ and the Unknown is only in the second
-    assert len(analyzer.counter) == 3
-    # the two different calls are +1 and -1, the additionall Unknown-0xEE is +1
-    assert analyzer.counter.total() == -1
+    # check if it detected the different CALL values
+    instruction_input_changes = analyzer.get_instructions_with_different_inputs()
+    assert len(instruction_input_changes) == 1
+    assert len(instruction_input_changes[0].input_changes) == 1
 
-    assert any(first_call_value in key.stack_inputs for key in analyzer.counter.keys())
-    assert any(second_call_value in key.stack_inputs for key in analyzer.counter.keys())
-    assert any(key.opcode == 0xEE for key in analyzer.counter.keys())
+    assert instruction_input_changes[0].address == "0xroot"
+    assert len(instruction_input_changes[0].input_changes) == 1
+    assert instruction_input_changes[0].input_changes[0].index == 2
+    assert instruction_input_changes[0].input_changes[0].first_value == first_call_value
+    assert instruction_input_changes[0].input_changes[0].second_value == second_call_value
+
+    # check if it detected the additional Unkown(opcode=0xEE) instruction in the 2nd trace
+    only_first_executions, only_second_executions = analyzer.get_instructions_only_executed_by_one_trace()
+
+    assert len(only_first_executions) == 0
+    assert len(only_second_executions) == 1
+    assert only_second_executions[0].opcode == 0xEE
+
+
+def test_instruction_input_analyzer_with_traces(sample_traces_path):
+    trace_normal_path = (
+        sample_traces_path
+        / "62a8b9ece30161692b68cbb5"
+        / "trace_normal"
+        / "0x5bc779188a1a4f701c33980a97e902fc097dc48393a01c61f363fce09f33e4a0.jsonl"
+    )
+    trace_attack_path = (
+        sample_traces_path
+        / "62a8b9ece30161692b68cbb5"
+        / "trace_attack"
+        / "0x5bc779188a1a4f701c33980a97e902fc097dc48393a01c61f363fce09f33e4a0.jsonl"
+    )
+
+    with open(trace_normal_path) as trace_normal_file, open(trace_attack_path) as trace_attack_file:
+        trace_normal_events = list(read_trace_file(trace_normal_file))
+        trace_attack_events = list(read_trace_file(trace_attack_file))
+
+        instructions_normal = list(parse_events(trace_normal_events))
+        instructions_attack = list(parse_events(trace_attack_events))
+
+        analyzer = InstructionInputAnalyzer()
+
+        for a, b in zip_longest(instructions_normal, instructions_attack):
+            analyzer.on_instructions(a, b)
+
+        only_first_executions, only_second_executions = analyzer.get_instructions_only_executed_by_one_trace()
+        assert len(only_first_executions) == 0
+        assert len(only_second_executions) == 178  # the trace files differ exactly by 178 lines
+
+        instruction_input_changes = analyzer.get_instructions_with_different_inputs()
+        assert len(instruction_input_changes) == 0  # no instruction was called with different stack inputs
--- traces_analyzer/analysis/InstructionInputAnalyzer.py
+++ traces_analyzer/analysis/InstructionInputAnalyzer.py
@@ -1,5 +1,8 @@
-from collections import Counter
+import dataclasses
+from collections import Counter, defaultdict
 from dataclasses import dataclass
+from itertools import zip_longest
+from typing import Iterable
 
 from typing_extensions import override
 
@@ -12,39 +15,187 @@ class InstructionKey:
     address: str
     program_counter: int
     opcode: int
+
+
+@dataclass(frozen=True)
+class InstructionKeyWithInputs(InstructionKey):
     stack_inputs: tuple[str, ...]
+
+    def without_inputs(self) -> InstructionKey:
+        return InstructionKey(
+            address=self.address,
+            program_counter=self.program_counter,
+            opcode=self.opcode,
+        )
+
+
+@dataclass(frozen=True)
+class InputChange:
+    index: int
+    first_value: str
+    second_value: str
+
+
+@dataclass(frozen=True)
+class InstructionInputChange:
+    """Same instruction with different inputs"""
+
+    address: str
+    program_counter: int
+    opcode: int
+
+    first_input: tuple[str, ...]
+    second_input: tuple[str, ...]
+    input_changes: list[InputChange]
+
+
+@dataclass
+class InstructionExecution:
+    address: str
+    program_counter: int
+    opcode: int
+    stack_inputs: tuple[str, ...]
+
+
+def to_instruction_execution(key: InstructionKeyWithInputs):
+    return InstructionExecution(
+        address=key.address,
+        program_counter=key.program_counter,
+        opcode=key.opcode,
+        stack_inputs=key.stack_inputs,
+    )
 
 
 class InstructionInputAnalyzer(TraceComparisonAnalyzer):
     """Analyze how the instruction inputs of two traces differ"""
 
-    counter: Counter[InstructionKey] = Counter()
+    def __init__(self) -> None:
+        super().__init__()
+        self._counter: Counter[InstructionKeyWithInputs] = Counter()
 
     @override
     def on_instructions(self, first_instruction: Instruction | None, second_instruction: Instruction | None):
         if first_instruction:
             key = to_key(first_instruction)
-            self.counter.update([key])
+            self._counter.update([key])
             self._delete_if_zero(key)
 
         if second_instruction:
             key = to_key(second_instruction)
-            self.counter.subtract([key])
+            self._counter.subtract([key])
             self._delete_if_zero(key)
-    
-    def _delete_if_zero(self, key: InstructionKey):
-        if self.counter[key] == 0:
-            self.counter.pop(key)
+
+    def _delete_if_zero(self, key: InstructionKeyWithInputs):
+        if self._counter[key] == 0:
+            self._counter.pop(key)
+
+    def get_instructions_only_executed_by_one_trace(
+        self,
+    ) -> tuple[list[InstructionExecution], list[InstructionExecution]]:
+        groups_first, groups_second = self._group_instructions_without_inputs()
+
+        only_first = get_additional_keys_first_group(groups_first, groups_second)
+        only_second = get_additional_keys_first_group(groups_second, groups_first)
+
+        return only_first, only_second
+
+    def get_instructions_with_different_inputs(self) -> list[InstructionInputChange]:
+        return [create_input_change(a, b) for a, b in self._match_instructions_with_different_inputs()]
+
+    def _match_instructions_with_different_inputs(
+        self,
+    ) -> Iterable[tuple[InstructionKeyWithInputs, InstructionKeyWithInputs]]:
+        groups_first, groups_second = self._group_instructions_without_inputs()
+
+        # match each key from the first trace to a key from the second
+        # ignore the inputs for the matching
+        for first_key_without_inputs, first_keys in groups_first.items():
+            for first_key in first_keys:
+                if groups_second.get(first_key_without_inputs):
+                    # pop from the start, so we try to match them in the same order
+                    second_key = groups_second[first_key_without_inputs].pop(0)
+
+                    yield (first_key, second_key)
+
+    def _group_instructions_without_inputs(
+        self,
+    ) -> tuple[
+        dict[InstructionKey, list[InstructionKeyWithInputs]], dict[InstructionKey, list[InstructionKeyWithInputs]]
+    ]:
+        """Split the instructions by trace and group them by their keys without inputs"""
+        keys_first = flatten(
+            [clone_key_with_inputs_times(key, abs(count)) for (key, count) in self._counter.items() if count > 0]
+        )
+        keys_second = flatten(
+            [clone_key_with_inputs_times(key, abs(count)) for (key, count) in self._counter.items() if count < 0]
+        )
+
+        groups_first = group_by_key_without_inputs(keys_first)
+        groups_second = group_by_key_without_inputs(keys_second)
+
+        return groups_first, groups_second
 
 
-def to_key(instruction: Instruction) -> InstructionKey:
+def flatten(matrix: list[list]) -> list:
+    return [item for row in matrix for item in row]
+
+
+def to_key(instruction: Instruction) -> InstructionKeyWithInputs:
     stack_inputs: tuple[str, ...] = ()
     if isinstance(instruction, StackInstruction):
         stack_inputs = instruction.stack_inputs
 
-    return InstructionKey(
+    return InstructionKeyWithInputs(
         address=instruction.call_frame.address,
         program_counter=instruction.program_counter,
         opcode=instruction.opcode,
         stack_inputs=stack_inputs,
+    )
+
+
+def clone_key_with_inputs_times(key: InstructionKeyWithInputs, n: int) -> list[InstructionKeyWithInputs]:
+    return [dataclasses.replace(key) for _ in range(n)]
+
+
+def group_by_key_without_inputs(
+    keys: list[InstructionKeyWithInputs],
+) -> dict[InstructionKey, list[InstructionKeyWithInputs]]:
+    groups: dict[InstructionKey, list[InstructionKeyWithInputs]] = defaultdict(list)
+
+    for key in keys:
+        groups[key.without_inputs()].append(key)
+
+    return groups
+
+
+def get_additional_keys_first_group(
+    groups_first: dict[InstructionKey, list[InstructionKeyWithInputs]],
+    groups_second: dict[InstructionKey, list[InstructionKeyWithInputs]],
+):
+    only_first: list[InstructionExecution] = []
+
+    for first_key_without_inputs, first_keys in groups_first.items():
+        second_keys = groups_second.get(first_key_without_inputs, [])
+        if len(first_keys) > len(second_keys):
+            additional_keys = first_keys[len(second_keys) :]
+            only_first.extend(to_instruction_execution(k) for k in additional_keys)
+
+    return only_first
+
+
+def create_input_change(
+    first_key: InstructionKeyWithInputs, second_key: InstructionKeyWithInputs
+) -> InstructionInputChange:
+    input_changes: list[InputChange] = []
+    for i, (first_input, second_input) in enumerate(zip_longest(first_key.stack_inputs, second_key.stack_inputs)):
+        if first_input != second_input:
+            input_changes.append(InputChange(index=i, first_value=first_input, second_value=second_input))
+
+    return InstructionInputChange(
+        address=first_key.address,
+        program_counter=first_key.program_counter,
+        opcode=first_key.opcode,
+        first_input=first_key.stack_inputs,
+        second_input=second_key.stack_inputs,
+        input_changes=input_changes,
     )
